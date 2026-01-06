e0d8afebcd1fc43c45e00319694081c630c5be62
swtrn <241483396+swtrn@users.noreply.github.com>
2026-01-06 13:26:48 -0300

    Shaders: changed fragment, vertex shader and added light shader.
    Modified fragment shader to support light, did minor modifications in
    vertex shader and added light shader that represents a light source.

--- Shaders/vertexShader.glsl
+++ Shaders/vertexShader.glsl
@@ -1,10 +1,10 @@
 #version 330 core
 layout (location = 0) in vec3 aPos;
 // layout (location = 1) in vec3 aColor;
-layout (location = 2) in vec2 aTexCoord;
+// layout (location = 2) in vec2 aTexCoord;
 
 // out vec3 ourColor;
-out vec2 TexCoord; 
+// out vec2 TexCoord; 
 
 uniform mat4 model;
 uniform mat4 view;
@@ -12,6 +12,6 @@ uniform mat4 projection;
 
 void main() {
   gl_Position = projection * view * model * vec4(aPos, 1.0);
-  TexCoord = aTexCoord; 
+  // TexCoord = aTexCoord; 
   // ourColor = aColor;
 }
