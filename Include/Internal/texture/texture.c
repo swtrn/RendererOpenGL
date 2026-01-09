3a25cf53ce11abd23aa25661f3bd9cb211c05aee
swtrn <241483396+swtrn@users.noreply.github.com>
2026-01-08 23:32:19 -0300

    Textures: Corrected LoadTexture function.
    Modified LoadTexture() to allow different color formats.
    Also modified .gitignore.

--- Include/Internal/texture/texture.c
+++ Include/Internal/texture/texture.c
@@ -16,14 +16,29 @@ void LoadTexture(int target, unsigned int *texture, char *texturePath) {
   stbi_set_flip_vertically_on_load(true);
   unsigned char *data = stbi_load(texturePath, &width, &height, &numberChannels,
                                   0); // Triangle vertices
-  if (data) {
-    // Generating texture
-    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB,
-                 GL_UNSIGNED_BYTE, data);
-    glGenerateMipmap(GL_TEXTURE_2D);
-  } else {
+
+  if (!data)
     printf("Failed to load texture.");
+
+  GLenum format;
+  switch (numberChannels) {
+  case 1:
+    format = GL_RED;
+  case 3:
+    format = GL_RGB;
+  case 4:
+    format = GL_RGBA;
   }
+
+  // Generating texture
+  glTexImage2D(target, 0, format, width, height, 0, format, GL_UNSIGNED_BYTE,
+               data);
+  glGenerateMipmap(target);
+
+  glTexParameteri(target, GL_TEXTURE_WRAP_S, GL_REPEAT);
+  glTexParameteri(target, GL_TEXTURE_WRAP_T, GL_REPEAT);
+  glTexParameteri(target, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
+  glTexParameteri(target, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
 
   // Freeing allocated image data
   stbi_image_free(data);
