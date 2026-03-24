3e43203198d6b7636edd9e403f2986c7b8dba363
swtrn <241483396+swtrn@users.noreply.github.com>
2026-03-23 23:07:50 -0300

    Light: Added spot light.
    Added spotlight with inner and outer cut off angles.

--- Shaders/Textured/multipleCasters.glsl
+++ Shaders/Textured/multipleCasters.glsl
@@ -24,11 +24,28 @@ struct PointLight {
   float quadratic;
 };
 
+struct SpotLight {
+  vec3 position;
+  vec3 direction;
+
+  float cutOff;
+  float outerCutOff;
+
+  vec3 ambient;
+  vec3 diffuse;
+  vec3 specular;
+
+  float constant;
+  float linear;
+  float quadratic;
+};
+
 #define POINT_LIGHTS_AMOUNT 4
 
 uniform Material material;
-uniform DirectionalLight directionalLight;
 uniform PointLight pointLights[POINT_LIGHTS_AMOUNT];
+uniform DirectionalLight directionalLight;
+uniform SpotLight spotLight;
 
 uniform vec3 viewPosition;
 
@@ -40,6 +57,7 @@ out vec4 FragColor;
 
 vec3 CalculateDirLight(DirectionalLight light, vec3 normal, vec3 viewDirection);
 vec3 CalculatePointLight(PointLight light, vec3 normal, vec3 viewDirection);
+vec3 CalculateSpotLight(SpotLight light, vec3 normal, vec3 viewDirection);
 
 void main() {
   // Calculating properties
@@ -54,7 +72,8 @@ void main() {
     result += CalculatePointLight(pointLights[i], normal, viewDirection);
   }
 
-  // TODO: Add spot light
+  // Spot light
+  result += CalculateSpotLight(spotLight, normal, viewDirection);
 
   FragColor = vec4(result, 1.0);
 };
@@ -100,9 +119,45 @@ vec3 CalculatePointLight(PointLight light, vec3 normal, vec3 viewDirection) {
   vec3 specular = light.specular * specularValue * specularColor;
 
   // Attenuation of light
-  float distance = length(distanceVector);
-  float attenuation = 1. / (light.constant + light.linear * distance + light.quadratic * pow(distance, 2));
+  float dist = length(distanceVector);
+  float attenuation = 1. / (light.constant + light.linear * dist + light.quadratic * pow(dist, 2));
 
   // Atennuating all types of light
   return (ambient + diffuse + specular) * attenuation;
+};
+
+vec3 CalculateSpotLight(SpotLight light, vec3 normal, vec3 viewDirection) {
+  // Calculating diffuse color
+  vec3 diffuseColor = vec3(texture(material.diffuse, TexCoord));
+
+  // Calculating light
+  vec3 distanceVector = light.position - FragPosition;
+  vec3 lightDirection = normalize(distanceVector);
+  float theta = dot(-lightDirection, normalize(light.direction));
+
+  // Ambient
+  vec3 ambient = light.ambient * diffuseColor;
+
+  // If object is not hit by spotlight
+  if (theta <= light.outerCutOff) return ambient;
+
+  // Calculating intensity
+  float eps = light.cutOff - light.outerCutOff;
+  float intensity = clamp((theta - light.outerCutOff) / eps, 0.0, 1.0);
+
+  // Diffuse
+  vec3 diffuse = light.diffuse * max(dot(lightDirection, normal), 0.0) * diffuseColor;
+
+  // Specular lighting
+  vec3 reflectDirection = reflect(-lightDirection, normal);
+
+  float specularValue = pow(max(dot(viewDirection, reflectDirection), 0.0), material.shine);
+  vec3 specularColor = vec3(texture(material.specular, TexCoord));
+  vec3 specular = light.specular * specularValue * specularColor;
+
+  // Attenuation of light
+  float dist = length(distanceVector);
+  float attenuation = 1. / (light.constant + light.linear * dist + light.quadratic * pow(dist, 2));
+
+  return ambient + (diffuse + specular) * intensity * attenuation;
 };
